3ff1f6834afd6e602069904f1f3a2a5542f486e5
marina-sviridova <marina.sviridova.w@gmail.com>
2026-05-22 16:51:03 +0200

    Initial project setup

---
+++ trackly/src/main/java/com/trackly/TracklyApplication.java
@@ -0,0 +1,11 @@
+package com.trackly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracklyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TracklyApplication.class, args);
+    }
+}
